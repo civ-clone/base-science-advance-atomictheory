e3b6e3aacb10900ccc06887d675b4841f0fa9812
dom111 <dom111@users.noreply.github.com>
2022-02-25 20:34:37 +0000

    Initial commit.

---
+++ AtomicTheory.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AtomicTheory = void 0;
+const Advance_1 = require("@civ-clone/core-science/Advance");
+class AtomicTheory extends Advance_1.default {
+}
+exports.AtomicTheory = AtomicTheory;
+exports.default = AtomicTheory;
+//# sourceMappingURL=AtomicTheory.js.map
---
+++ registerAdvance.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const AdvanceRegistry_1 = require("@civ-clone/core-science/AdvanceRegistry");
+const AtomicTheory_1 = require("./AtomicTheory");
+AdvanceRegistry_1.instance.register(AtomicTheory_1.default);
+//# sourceMappingURL=registerAdvance.js.map
